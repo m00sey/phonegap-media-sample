4f09a78742b7c967c56ddc790c340731c321e2ef
Kevin Griffin <griffin.kev@gmail.com>
2011-08-09 23:26:15 -0400

    first

---
+++ src/com/moominland/MediaSample.java
@@ -0,0 +1,14 @@
+package com.moominland;
+
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class MediaSample extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
